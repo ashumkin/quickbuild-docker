aea2eebb2ef35f8b61328efd369a6c762803cf29
Alexey Shumkin <Alex.Crezoff@gmail.com>
2016-02-09 13:09:02 +0300

    fixed: backup script does not work when run in cron
    PATH does not contain /usr/local/bin/ where docker-compose is.

--- backup-qb-db.sh
+++ backup-qb-db.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CONTAINER_NAME=$(docker-compose ps | grep qb-database | cut -d' ' -f1)
+CONTAINER_NAME=$(/usr/local/bin/docker-compose ps | grep qb-database | cut -d' ' -f1)
 docker exec "$CONTAINER_NAME" \
     bash -c 'mysqldump -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" --dump-date --single-transaction --extended-insert > "$MYSQL_BACKUP_DIR/$MYSQL_DATABASE.sql"'
 #docker run -it --link "$CONTAINER_NAME":mysql --rm mariadb sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -u"$MYSQL_ENV_MYSQL_USER" -p"$MYSQL_ENV_MYSQL_PASSWORD" "$MYSQL_ENV_MYSQL_DATABASE"'
