1a61e0036425dfdcde000d4ef8dd8f92b5ec85bf
Alexey Shumkin <Alex.Crezoff@gmail.com>
2018-01-09 13:50:28 +0300

    improve: name MySQL backup with a project name

--- backup-qb-db.sh
+++ backup-qb-db.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+PROJECT=$1
+PROJECT_B=
+PROJECT_P=
+if test -n "$PROJECT"
+then
+    PROJECT_B="-$PROJECT"
+    PROJECT_P="-p $PROJECT"
+fi
 CONTAINER_NAME=qb-database
-/usr/local/bin/docker-compose exec -T "$CONTAINER_NAME" \
-    bash -c 'mysqldump -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" --dump-date --single-transaction --extended-insert > "$MYSQL_BACKUP_DIR/$MYSQL_DATABASE.sql"'
+/usr/local/bin/docker-compose $PROJECT_P exec -T "$CONTAINER_NAME" \
+    bash -c "mysqldump -u\"\$MYSQL_USER\" -p\"\$MYSQL_PASSWORD\" \"\$MYSQL_DATABASE\" --dump-date --single-transaction --extended-insert > \"\$MYSQL_BACKUP_DIR/\$MYSQL_DATABASE$PROJECT_B.sql\""
