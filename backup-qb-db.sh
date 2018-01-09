6d6f2acda503562752df36c9301f214200e79b44
Alexey Shumkin <Alex.Crezoff@gmail.com>
2018-01-09 13:22:42 +0300

    improve: use `docker-compose exec` rather than `docker exec`

--- backup-qb-db.sh
+++ backup-qb-db.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-CONTAINER_NAME=$(/usr/local/bin/docker-compose ps | grep qb-database | cut -d' ' -f1)
-docker exec -t "$CONTAINER_NAME" \
+CONTAINER_NAME=qb-database
+/usr/local/bin/docker-compose exec -T "$CONTAINER_NAME" \
     bash -c 'mysqldump -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" --dump-date --single-transaction --extended-insert > "$MYSQL_BACKUP_DIR/$MYSQL_DATABASE.sql"'
