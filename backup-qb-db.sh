41c747c7efdfd4a2f1ad276a81f58527409fa04e
Alexey Shumkin <Alex.Crezoff@gmail.com>
2018-01-12 19:45:02 +0300

    improve: add date-time to a DB backup filename

--- backup-qb-db.sh
+++ backup-qb-db.sh
@@ -3,6 +3,7 @@
 PROJECT=$1
 PROJECT_B=
 PROJECT_P=
+DATE=$(date +%F-%H-%M-%S)
 if test -n "$PROJECT"
 then
     PROJECT_B="-$PROJECT"
@@ -10,4 +11,4 @@ then
 fi
 CONTAINER_NAME=qb-database
 /usr/local/bin/docker-compose $PROJECT_P exec -T "$CONTAINER_NAME" \
-    bash -c "mysqldump -u\"\$MYSQL_USER\" -p\"\$MYSQL_PASSWORD\" \"\$MYSQL_DATABASE\" --dump-date --single-transaction --extended-insert > \"\$MYSQL_BACKUP_DIR/\$MYSQL_DATABASE$PROJECT_B.sql\""
+    bash -c "mysqldump -u\"\$MYSQL_USER\" -p\"\$MYSQL_PASSWORD\" \"\$MYSQL_DATABASE\" --dump-date --single-transaction --extended-insert > \"\$MYSQL_BACKUP_DIR/\$MYSQL_DATABASE$PROJECT_B-$DATE.sql\""
